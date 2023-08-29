f9006bf4104f7ccdff6b8f1362d824edece4111e
jed4793 <tjsdlr04@gmail.com>
2023-08-30 01:11:05 +0900

    버그해결
    문제:
    구분 가능

--- src/main/java/org/sunik/statsredirect/handler/InventoryHandler.java
+++ src/main/java/org/sunik/statsredirect/handler/InventoryHandler.java
@@ -23,45 +23,48 @@ public class InventoryHandler implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
-        // 클릭된 인벤토리
         Inventory clickedInventory = event.getClickedInventory();
-        int slot = event.getSlot(); // 클릭한 슬롯 번호
-
-        // 클릭된 인벤토리가 없거나 인벤토리가 플레이어의 열린 상단 인벤토리와 다를 경우 무시
+        int slot = event.getSlot();
         if (clickedInventory == null || !clickedInventory.equals(player.getOpenInventory().getTopInventory())) {
             return;
         }
 
-        // 클릭된 아이템
         ItemStack clickedItem = event.getCurrentItem();
-        // 클릭된 아이템의 메타 데이터
+        plugin.getLogger().info("3");
+
+        if (clickedItem == null) {
+            return; // 클릭된 아이템이 없는 경우 무시
+        }
+
         ItemMeta itemMeta = clickedItem.getItemMeta();
-        // 클릭한 아이템 이름이 (힘, 체력, 민첩, 행운) 이고 아이템 종류가 (빨강, 초록, 하늘, 노랑)색 유리판 이 아닐 경우 무시
-        boolean str = itemMeta.getDisplayName().equals(ChatColor.RED + (ChatColor.BOLD + "힘")) && clickedItem.getType() != Material.RED_STAINED_GLASS_PANE;
-        boolean con = itemMeta.getDisplayName().equals(ChatColor.GREEN + (ChatColor.BOLD + "체력")) && clickedItem.getType() != Material.GREEN_STAINED_GLASS_PANE;
-        boolean dex = itemMeta.getDisplayName().equals(ChatColor.AQUA + (ChatColor.BOLD + "민첩")) && clickedItem.getType() != Material.LIGHT_BLUE_STAINED_GLASS_PANE;
-        boolean luck = itemMeta.getDisplayName().equals(ChatColor.GOLD + (ChatColor.BOLD + "행운")) && clickedItem.getType() != Material.YELLOW_STAINED_GLASS_PANE;
+
+        if (itemMeta == null) {
+            return; // 아이템 메타 데이터가 없는 경우 무시
+        }
+
+        boolean str = itemMeta.getDisplayName().equals(ChatColor.RED + (ChatColor.BOLD + "힘")) && clickedItem.getType() == Material.RED_STAINED_GLASS_PANE;
+        boolean con = itemMeta.getDisplayName().equals(ChatColor.GREEN + (ChatColor.BOLD + "체력")) && clickedItem.getType() == Material.GREEN_STAINED_GLASS_PANE;
+        boolean dex = itemMeta.getDisplayName().equals(ChatColor.AQUA + (ChatColor.BOLD + "민첩")) && clickedItem.getType() == Material.LIGHT_BLUE_STAINED_GLASS_PANE;
+        boolean luck = itemMeta.getDisplayName().equals(ChatColor.GOLD + (ChatColor.BOLD + "행운")) && clickedItem.getType() == Material.YELLOW_STAINED_GLASS_PANE;
+        plugin.getLogger().info(str + "," + con + "," + dex + "," + luck);
         if (str && con && dex && luck) {
             return;
         }
 
-        // 아이템 원래 자리로 돌려놓기
-        if (slot >= 0 && slot < player.getOpenInventory().getTopInventory().getSize()) {
-            player.getOpenInventory().getTopInventory().setItem(slot, clickedItem);
-        }
-
-        // 손의 아이템 제거
-        event.setCurrentItem(null);
         if (str) {
+            event.setCurrentItem(null);
             addStr();
             player.performCommand("stats");
         } else if (con) {
+            event.setCurrentItem(null);
             addCon(player);
             player.performCommand("stats");
         } else if (dex) {
+            event.setCurrentItem(null);
             addDex();
             player.performCommand("stats");
         } else if (luck) {
+            event.setCurrentItem(null);
             addLuck();
             player.performCommand("stats");
         }
